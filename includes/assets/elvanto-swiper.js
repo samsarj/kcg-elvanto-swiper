96772a6927f72076de32fc13906ecc0ccbd17223
samsarj <samsarj@gmail.com>
2025-07-27 13:54:19 +0100

    added mousewheel and keyboard control

--- includes/assets/elvanto-swiper.js
+++ includes/assets/elvanto-swiper.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   var swiper = new Swiper(".swiper-container", {
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     effect: "cards",
     grabCursor: true,
+    mousewheel: {
+      enabled: true,
+      forceToAxis: true,
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
     cardsEffect: {
-    //   slideShadows: false,
-    //   perSlideRotate: 0,
       perSlideOffset: 80,
     },
   });
